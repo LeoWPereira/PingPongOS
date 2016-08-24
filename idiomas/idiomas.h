bd15ca8f91a9ba6f80edd9c603beaaa827553abb
Leonardo Winter Pereira <leonardo.wpereira@hotmail.com>
2016-08-23 21:03:01 -0300

    cara, viadagens

---
+++ idiomas/idiomas.h
@@ -0,0 +1,17 @@
+/*
+* Ping Pong OS
+* idiomas.h
+* Projeto da disciplina Sistemas Operacionais - UTFPR - 2016.02
+* Professor: Copetti
+* Alunos: Leonardo Winter Pereira (leonardowinterpereira@gmail.com)
+*         Lucas Zimmermann Cordeiro ()
+*/
+
+/////////////////////////////////////////////////////////////////////
+//                      Diretivas de Compilação                    //
+/////////////////////////////////////////////////////////////////////
+
+#ifndef __IDIOMAS__
+#define __IDIOMAS__
+#endif
+
